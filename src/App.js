fc5be6655026efbcd18e0f9a28fd91db0adbd8f3
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2020-03-23 18:56:30 +0500

    Boilerplate Code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Switch,
+  Route,
+  Link,
+  withRouter
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>NovaSoft</h1>
+    </div>
+  );
+}
+
+export default withRouter(App);
